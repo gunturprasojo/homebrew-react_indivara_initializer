1fb63ca88fed57e79abf96049cdbf7a0484d8beb
Guntur Budi <gunturbudi@Gunturs-MacBook-Pro.local>
2025-07-23 00:40:26 +0700

    Update Packages

--- react_indivara_initializer.rb
+++ react_indivara_initializer.rb
@@ -1,26 +1,14 @@
 class ReactIndivaraInitializer < Formula
   desc "React Generator for Indivara Project"
   homepage "www.indivaragroup.com"
-  url "https://github.com/gunturprasojo/react_indivara_initializer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "6953703aca9d1f95f7b5d68cd4bd83deaeddbcd8dc2d941860232456816b2d76"
+  url "https://github.com/gunturprasojo/react_indivara_initializer/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "185bc63d299f1d7bd5915458d5906ce3833247993107eb09e8c6319b72261179"
   license "MIT"
 
   def install
-    libexec.install Dir["*"]
     bin.install "react_indivara"
     bin.install "react_indivara_project"
     bin.install "react_indivara_page"
-
-    # Rewrite each bin script to point to libexec
-    bin.find.each do |path|
-      next unless path.file? && path.executable?
-
-      script_name = path.basename
-      (bin/script_name).write <<~EOS
-        #!/bin/bash
-        exec ruby "#{libexec}/#{script_name}" "$@"
-      EOS
-    end
   end
   
 end
