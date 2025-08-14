5a8fbe282c2b699ca49229c871d4f84294723c18
Guntur Prasojo <gunturbudi@Gunturs-Mac-mini.local>
2025-08-14 22:53:17 +0700

    Update Homebrew

--- react_indivara_initializer.rb
+++ react_indivara_initializer.rb
@@ -1,15 +1,15 @@
 class ReactIndivaraInitializer < Formula
   desc "React Generator for Indivara Project"
   homepage "https://www.indivaragroup.com"
-  url "https://github.com/gunturprasojo/react_indivara_initializer/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "185bc63d299f1d7bd5915458d5906ce3833247993107eb09e8c6319b72261179"
+  url "https://github.com/gunturprasojo/react_indivara_initializer/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "5516abac7cf0908eff7ad4dd0184ff6c21028a8e9af5bc717a18d746d44bccc1"
   license "MIT"
 
   def install
     libexec.install Dir["*"]
 
     # Create wrapper scripts in /usr/local/bin that call the ruby files in libexec
-    %w[react_indivara react_indivara_project react_indivara_page].each do |script|
+    %w[react_indivara react_indivara_project react_indivara_page react_indivara_section].each do |script|
       (bin/script).write <<~EOS
         #!/bin/bash
         exec ruby "#{libexec}/#{script}" "$@"
